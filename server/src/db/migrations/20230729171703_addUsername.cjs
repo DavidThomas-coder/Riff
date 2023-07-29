90d0df0f6bf130920646a54cacd1f9891ec052f4
RipNasty <det625@gmail.com>
2023-07-29 13:19:45 -0400

    made migrations

---
+++ server/src/db/migrations/20230729171703_addUsername.cjs
@@ -0,0 +1,21 @@
+/**
+ * @typedef {import("knex")} Knex
+ */
+
+/**
+ * @param {Knex} knex
+ */
+exports.up = async (knex) => {
+    return knex.schema.alterTable("users", (table) => {
+        table.string("username").notNullable()
+    })
+}
+
+/**
+ * @param {Knex} knex
+ */
+exports.down = (knex) => {
+    return knex.schema.alterTable("users", (table) => {
+        table.dropColumn("username")
+    })
+}
