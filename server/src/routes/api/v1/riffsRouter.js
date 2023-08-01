2a79b2dd964d597a2d7c71017e6ec60f7b000361
RipNasty <det625@gmail.com>
2023-08-01 13:18:23 -0400

    added a riffs GET

--- server/src/routes/api/v1/riffsRouter.js
+++ server/src/routes/api/v1/riffsRouter.js
@@ -3,6 +3,16 @@ import { Riff } from "../../../models/index.js";
 
 const riffsRouter = express.Router();
 
+riffsRouter.get("/", async (req, res) => {
+    try {
+        const allRiffs = await Riff.query();
+        return res.status(200).json({ riffs: allRiffs });
+        } catch (error) {
+        console.error("Error fetching all riffs:", error);
+        return res.status(500).json({ error: "Internal Server Error" });
+        }
+});
+
 // Endpoint to save a riff
 riffsRouter.post("/", async (req, res) => {
     try {
