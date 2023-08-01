72f1e7e6a8efc6aa6866f6c5519a2c7862a0a854
RipNasty <det625@gmail.com>
2023-08-01 13:33:46 -0400

    added relations to Riff model

--- server/src/models/Riff.js
+++ server/src/models/Riff.js
@@ -15,6 +15,29 @@ class Riff extends Model {
         }
     }
 
+    static get relationMappings() {
+        const {Prompt, User} = require("./index.js")
+
+        return {
+            users: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: User,
+                join: {
+                    from: "riffs.userId",
+                    to: "users.id"
+                }
+            },
+            prompts: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: Prompt,
+                join: {
+                    from: "riffs.promptId",
+                    to: "prompts.id"
+                }
+            }
+        }
+    }
+
 }
 
 module.exports = Riff
