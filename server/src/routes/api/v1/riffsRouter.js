e0633a07f821900c859de4903bd66e579a7527c3
RipNasty <det625@gmail.com>
2023-08-01 14:57:21 -0400

    changing the way prompts are fetched

--- server/src/routes/api/v1/riffsRouter.js
+++ server/src/routes/api/v1/riffsRouter.js
@@ -15,18 +15,18 @@ riffsRouter.get("/", async (req, res) => {
 
 riffsRouter.post("/", async (req, res) => {
     try {
-      const { riffBody } = req.body;
-      console.log("req.body is:", req.body)
-      const userId = req.user.userId; // Assuming the authenticated user's ID is available in req.user
-  
-      // Insert the riff with the associated userId
-      const riff = await Riff.query().insert({ riffBody, userId });
-  
-      return res.status(201).json({ riff });
-    } catch (error) {
-      console.error("Error saving riff:", error);
-      return res.status(500).json({ error: "Internal Server Error" });
+        const { riffBody, userId } = req.body;
+        console.log("req.body is:", req.body)
+        // const userId = req.user.userId; // Assuming the authenticated user's ID is available in req.user
+    
+        // Insert the riff with the associated userId
+        const riff = await Riff.query().insert({ riffBody, userId });
+    
+        return res.status(201).json({ riff });
+        } catch (error) {
+        console.error("Error saving riff:", error);
+        return res.status(500).json({ error: "Internal Server Error" });
     }
-  });
+});
 
 export default riffsRouter
