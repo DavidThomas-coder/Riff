067a230369581d23546b65db67a4c9d4b67056a2
RipNasty <det625@gmail.com>
2023-08-02 13:03:19 -0400

    other riffs have usernames

--- client/src/components/OtherRiffTile.js
+++ client/src/components/OtherRiffTile.js
@@ -5,6 +5,7 @@ const OtherRiffTile = ({ riff, userId }) => {
 
     useEffect(() => {
         console.log("Received userId:", userId); // Log the userId to verify if it's correct
+        console.log("Received riff:", riff)
         const fetchUsername = async () => {
         try {
             // Fetch the username from the backend using the userId
@@ -12,9 +13,10 @@ const OtherRiffTile = ({ riff, userId }) => {
             if (!response.ok) {
             throw new Error(`${response.status} (${response.statusText})`);
             }
-            const { username } = await response.json();
-            console.log("Username for user ID", userId, ":", username);
-            setUsername(username);
+            const data = await response.json();
+            console.log("Data:", data)
+            console.log("Username for user ID", userId, ":", data.user.username);
+            setUsername(data.user.username);
         } catch (error) {
             // If an error occurs while fetching the username, handle it gracefully
             console.error("Error fetching the username:", error);
