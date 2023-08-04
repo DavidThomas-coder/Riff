3e02e7e34e2877a623c075ac889f099a1679ae7d
RipNasty <det625@gmail.com>
2023-08-04 15:29:37 -0400

    commit

--- client/src/components/RiffForm.js
+++ client/src/components/RiffForm.js
@@ -1,22 +1,34 @@
 import React from "react";
 
 const RiffForm = ({ prompt, userAnswer, onUserAnswerChange, onSubmit }) => {
-    return (
-        <form onSubmit={onSubmit}>
-            <label>
-                <p>{prompt}</p>
-            </label>
-            <label>
-                Your Answer:
-                <input
-                    type="text"
-                    value={userAnswer}
-                    onChange={onUserAnswerChange}
-                />
-            </label>
-            <button type="submit">Submit</button>
-        </form>
-    );
+  const handleChange = (event) => {
+    console.log("User Answer Change:", event.target.value);
+    onUserAnswerChange(event);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log("Form Submitted!");
+    onSubmit(event);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        <p>{prompt}</p>
+      </label>
+      <label>
+        Your Answer:
+        <input
+          type="text"
+          value={userAnswer}
+          onChange={handleChange}
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
 
 export default RiffForm;
+
