39aea27b0bb1c87779fe224944e57cb86c8d4be0
RipNasty <det625@gmail.com>
2023-08-05 23:21:47 -0400

    updated user model to store last submitted riff date

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -31,6 +31,7 @@ class User extends uniqueFunc(Model) {
       properties: {
         email: { type: "string", pattern: "^\\S+@\\S+\\.\\S+$" },
         cryptedPassword: { type: "string" },
+        lastSubmittedRiffDate: { type: "string", format: "date" },
       },
     };
   }
