df8aca2670a62d1b6db53023d20af53fc7aa888b
RipNasty <det625@gmail.com>
2023-08-06 15:58:23 -0400

    alternate users post made

--- server/src/routes/api/v1/usersRouter.js
+++ server/src/routes/api/v1/usersRouter.js
@@ -46,4 +46,29 @@ usersRouter.post("/", async (req, res) => {
   }
 });
 
+// usersRouter.post("/", async (req, res) => {
+//   const { email, username, password } = req.body;
+
+//   try {
+//     const persistedUser = await User.query().insertAndFetch({ email, username, password });
+
+//     // If the user is successfully created or fetched, update the lastSubmittedRiffDate
+//     if (persistedUser) {
+//       // Update the lastSubmittedRiffDate for the user on the server-side
+//       const lastSubmittedRiffDate = new Date().toISOString().slice(0, 10);
+//       const updatedUser = await User.query().patchAndFetchById(persistedUser.id, { lastSubmittedRiffDate });
+
+//       return req.login(updatedUser, () => {
+//         return res.status(201).json({ user: updatedUser });
+//       });
+//     } else {
+//       return res.status(404).json({ error: "User not found" });
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(422).json({ errors: error });
+//   }
+// });
+
+
 export default usersRouter;
