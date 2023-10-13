02bd7fc86cd0e2da0550476cb45449a59d4d985a
RipNasty <det625@gmail.com>
2023-10-13 17:12:30 -0400

    console logs

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -89,12 +89,12 @@ const HomePage = (props) => {
                     promptId: currentPrompt.id,
                 }));
             } else {
-                const defaultPrompt = "Here's today's prompt:";
+                const defaultPrompt = "Today's prompt:";
                 setHomepage((prevHomepage) => ({ ...prevHomepage, prompt: defaultPrompt }));
             }
         } catch (error) {
             console.error("Error fetching the current prompt:", error);
-            const defaultPrompt = "Here's today's prompt:";
+            const defaultPrompt = "Today's prompt:";
             setHomepage((prevHomepage) => ({ ...prevHomepage, prompt: defaultPrompt }));
         }
     };
